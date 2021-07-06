e0b0fad0f267e75b7fc829f88e4aa2d0c56a1d4f
Shaurya Vardhan <gsshaurya@gmail.com>
2021-07-06 23:16:38 +0530

    refactor: used standard for formatting

--- build.js
+++ build.js
@@ -1,44 +1,44 @@
-const chalk = require("chalk");
-const boxen = require("boxen");
-const fs = require("fs");
-const path = require("path");
+const chalk = require('chalk')
+const boxen = require('boxen')
+const fs = require('fs')
+const path = require('path')
 
 const options = {
   padding: 1,
   margin: 1,
-  borderStyle: "round",
-};
+  borderStyle: 'round'
+}
 
 const data = {
-  name: chalk.white("                  Shaurya Vardhan Singh"),
-  handle: chalk.white.bold("wulfor"),
-  twitter: chalk.gray("https://twitter.com/") + chalk.red("wulforr"),
-  github: chalk.gray("https://github.com/") + chalk.green("wulforr"),
+  name: chalk.white('                  Shaurya Vardhan Singh'),
+  handle: chalk.white.bold('wulfor'),
+  twitter: chalk.gray('https://twitter.com/') + chalk.red('wulforr'),
+  github: chalk.gray('https://github.com/') + chalk.green('wulforr'),
   linkedin:
-    chalk.gray("https://linkedin.com/in/") +
-    chalk.blue("shaurya-vardhan-singh"),
-  labelTwitter: chalk.white.bold("    Twitter:"),
-  labelGitHub: chalk.white.bold("     GitHub:"),
-  labelLinkedIn: chalk.white.bold("   LinkedIn:"),
+    chalk.gray('https://linkedin.com/in/') +
+    chalk.blue('shaurya-vardhan-singh'),
+  labelTwitter: chalk.white.bold('    Twitter:'),
+  labelGitHub: chalk.white.bold('     GitHub:'),
+  labelLinkedIn: chalk.white.bold('   LinkedIn:'),
   descriptionLine1: chalk.white(
-    "Hi, I am Shaurya a Software Developer with a knack for building things."
+    'Hi, I am Shaurya a Software Developer with a knack for building things.'
   ),
   descriptionLine2: chalk.white(
-    "     I created this tool for introducing myself in a fun way"
+    '     I created this tool for introducing myself in a fun way'
   ),
   descriptionLine3: chalk.white(
-    "          Below are some links to get in touch with me"
-  ),
-};
+    '          Below are some links to get in touch with me'
+  )
+}
 
-const newline = "\n";
-const heading = `${data.name} / ${data.handle}`;
-const twittering = `${data.labelTwitter}  ${data.twitter}`;
-const githubing = `${data.labelGitHub}  ${data.github}`;
-const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`;
-const descriptioningLine1 = `${data.descriptionLine1}`;
-const descriptioningLine2 = `${data.descriptionLine2}`;
-const descriptioningLine3 = `${data.descriptionLine3}`;
+const newline = '\n'
+const heading = `${data.name} / ${data.handle}`
+const twittering = `${data.labelTwitter}  ${data.twitter}`
+const githubing = `${data.labelGitHub}  ${data.github}`
+const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
+const descriptioningLine1 = `${data.descriptionLine1}`
+const descriptioningLine2 = `${data.descriptionLine2}`
+const descriptioningLine3 = `${data.descriptionLine3}`
 
 const output =
   heading +
@@ -55,9 +55,9 @@ const output =
   newline +
   githubing +
   newline +
-  linkedining;
+  linkedining
 
 fs.writeFileSync(
-  path.join(__dirname, "bin/output"),
+  path.join(__dirname, 'bin/output'),
   chalk.yellowBright(boxen(output, options))
-);
+)
