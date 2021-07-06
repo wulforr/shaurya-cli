da7c83902f675952e7e96f70bc3c9ebfa14837f5
Shaurya Vardhan <gsshaurya@gmail.com>
2021-07-06 23:36:38 +0530

    feat: add loading state and spinner

--- bin/card.js
+++ bin/card.js
@@ -1,9 +1,17 @@
 #!/usr/bin/env node
 // 👆 Used to tell Node.js that this is a CLI tool
 
-'use strict'
+"use strict";
 
-const fs = require('fs')
-const path = require('path')
-const output = fs.readFileSync(path.join(__dirname, 'output'), 'utf8')
-console.log(output)
+const fs = require("fs");
+const path = require("path");
+const ora = require("ora");
+const output = fs.readFileSync(path.join(__dirname, "output"), "utf8");
+
+const spinner = ora("Loading the information about shaurya").start();
+spinner.color = "green";
+
+setTimeout(() => {
+  spinner.stop();
+  console.log(output);
+}, 1000);
